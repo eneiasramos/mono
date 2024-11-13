517880c7e71cd5791537717c34c64fee1b212f21
Enéias Ramos de Melo <eneiasramos@users.noreply.github.com>
2024-11-12 21:39:15 -0300

    WSL

---
+++ wsl.sh
@@ -0,0 +1,3 @@
+sudo gpg --homedir /tmp --no-default-keyring --keyring /usr/share/keyrings/mono-official-archive-keyring.gpg --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
+echo "deb [signed-by=/usr/share/keyrings/mono-official-archive-keyring.gpg] https://download.mono-project.com/repo/ubuntu stable-bionic main" | sudo tee    /etc/apt/sources.list.d/mono-official-stable.list
+echo "deb [signed-by=/usr/share/keyrings/mono-official-archive-keyring.gpg] https://download.mono-project.com/repo/ubuntu stable-focal  main" | sudo tee -a /etc/apt/sources.list.d/mono-official-stable.list
